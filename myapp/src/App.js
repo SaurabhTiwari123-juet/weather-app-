89c43500f3443ea8b868518527b5101f153afd6a
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2023-01-16 05:39:00 +0530

    weather app introduction

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import 'Navbar' from './src/components/Navbar.js';
+function App() {
+	return (
+		<>
+			<Navbar />
+		</>
+	);
+}
+
+export default App;
---
+++ myapp/src/components/Navbar.js
@@ -0,0 +1,19 @@
+/** @format */
+import "./Searchbar";
+import "./Navbar.css";
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div>
+			{/* <h1 className='one'>this is nav</h1>
+			<button className='btn'> text</button> */}
+
+			<button type='button' className='btn btn-primary'>
+				Primary
+			</button>
+		</div>
+	);
+};
+
+export default Navbar;
